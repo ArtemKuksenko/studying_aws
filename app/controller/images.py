85be6460da8c3a777312152da08d63f903cb108c
Artem Kuksenko <artem.kuksenko@akvelon.com>
2023-04-02 23:47:02 +0600

    process_image

--- app/controller/images.py
+++ app/controller/images.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image
 
 
-def process_image(file: bytes):
+def process_image(file: bytes) -> bytes:
     image = Image.open(BytesIO(file))
     frames = [
         image.rotate(degree)
@@ -11,11 +11,8 @@ def process_image(file: bytes):
     ]
     frame_one = frames[0]
     res_image = BytesIO()
+
     frame_one.save(
-        "my_awesome.gif", format="GIF", append_images=frames, save_all=True, duration=20, loop=0
+        res_image, format="GIF", append_images=frames, save_all=True, duration=40, loop=0
     )
-    frame_one.save(
-        res_image, format="GIF", append_images=frames, save_all=True, duration=20, loop=0
-    )
-    # rotated_image = image.rotate(180)
-    # rotated_image.save("geeks.jpg")
+    return res_image.getvalue()
